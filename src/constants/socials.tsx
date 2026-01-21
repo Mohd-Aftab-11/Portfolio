47866e1f9cf3cb36475420099f0569d779ddb138
Mohd. Aftab <ma.aftab2003@gmail.com>
2026-01-21 14:42:26 +0530

    first commit

---
+++ src/constants/socials.tsx
@@ -0,0 +1,53 @@
+import {
+  IconBrandLinkedin,
+  IconBrandGithub,
+  IconBrandLeetcode,
+  IconBrandCodecov,
+  IconBrandPushbullet,
+  IconBrandStackoverflow,
+} from "@tabler/icons-react";
+import { SiGeeksforgeeks } from "react-icons/si";
+
+export const socials = [
+  {
+    href: "https://www.linkedin.com/in/mohd-aftab-76039a324/",
+    label: "LinkedIn",
+    icon: IconBrandLinkedin,
+  },
+
+  {
+    href: "https://github.com/Mohd-Aftab-11",
+    label: "Github",
+    icon: IconBrandGithub,
+  },
+
+  {
+    href: "https://geeksforgeeks.org/user/mohdaftab",
+    label: "GeeksforGeeks",
+    icon: SiGeeksforgeeks,
+  },
+
+  {
+    href: "https://leetcode.com/u/Mohd_Aftab_11/",
+    label: "LeetCode",
+    icon: IconBrandLeetcode,
+  },
+
+  {
+    href: "hackerrank.com/profile/ma_aftab200",
+    label: "Hackerrank",
+    icon: IconBrandPushbullet,
+  },
+
+  {
+    href: "https://www.codechef.com/users/mohd_aftab_11",
+    label: "CodeChef",
+    icon: IconBrandCodecov,
+  },
+
+  {
+    href: "https://stackoverflow.com/users/32222711/mohd-aftab",
+    label: "Stackoverflow",
+    icon: IconBrandStackoverflow,
+  },
+];
---
+++ src/constants/timeline.tsx
@@ -0,0 +1,23 @@
+export const timeline = [
+  {
+    company: "Pineapple Consultancy Services",
+    title: "Software Trainee",
+    date: "2024",
+    description: "Domain: Frontend with asp.net",
+    responsibilities: [
+      "Worked on developing scalable web applications",
+      "Created Hospital Management system web application.",
+    ],
+  },
+  {
+    company: "Analyze Infotech",
+    title: "Software Intern",
+    date: "2025",
+    description: "Domain: Web Development",
+    responsibilities: [
+      "Worked on developing scalable web applications",
+      "Learned Full Stack Java , JavaScript, HTML & CSS, and MySQL",
+      "Created full stack project: E-Learning website",
+    ],
+  },
+];
